9d22b2b92ed3bc79017de5cbe89cb77a75e9cb7f
nataliabaptista <natalia.mendes.baptista@hotmail.com>
2021-11-17 16:22:18 -0300

    Adição arq procfile

--- api.py
+++ api.py
@@ -1,3 +1,13 @@
-from flask import Flask
+from flask import Flask, jsonify
 from pymongo import MongoClient
+import os
+from flask_cors import CORS
 
+app = Flask(__name__)
+CORS(app)
+
+
+# Start flask program
+if __name__=="__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
