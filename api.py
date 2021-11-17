6f5651b8d395975d43a5928fb784e53ce7646ceb
nataliabaptista <natalia.mendes.baptista@hotmail.com>
2021-11-17 17:16:20 -0300

    teste2

--- api.py
+++ api.py
@@ -1,10 +1,10 @@
 from flask import Flask, jsonify
 from pymongo import MongoClient
 import os
-from flask_cors import CORS
+#from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
+#CORS(app)
 
 
 @app.route('/')
